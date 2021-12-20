cc93b6d0e07277331d149bb18babb71427e5ce2f
YAMANE Nana <yamane@bitnauts.co.jp>
2021-12-20 17:04:58 +0900

    初期化オプションの凡例を簡素化

--- var/conf/sentry.php
+++ var/conf/sentry.php
@@ -4,22 +4,10 @@ use Pj8\SentryModule\ExcludeSampler;
 
 // \Sentry\init の引数になるオプション配列
 return [
-    'dsn' => getenv('SENTRY_DSN') ?: null,
-    'environment' => getenv('APP_ENV') ?: 'unknown',
-
-    // エラーサンプリングレート
-    // @see https://docs.sentry.io/platforms/php/configuration/options/#sample-rate
-    'sample_rate' => (float) getenv('SENTRY_ERROR_SAMPLE_RATE'),
-
-    // パフォーマンスサンプリングレートを固定値にする場合
-    // @see https://docs.sentry.io/platforms/php/configuration/options/#traces-sample-rate
-    //'traces_sample_rate' => (float) getenv('SENTRY_PERFORMANCE_SAMPLE_RATE'),
-
-    // パフォーマンスサンプリングをコールバック関数で決める場合
-    // @see https://docs.sentry.io/platforms/php/configuration/options/#traces-sampler
+    'dsn' => null,
     'traces_sampler' => [
         new ExcludeSampler(
-            (float) getenv('SENTRY_PERFORMANCE_SAMPLER_RATE'),
+            0.0,
             // パフォーマンス計測をしたくないリクエストURIの配列
             ['/ignoreRequestUri.php']
         ),
